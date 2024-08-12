0996c3f51d0c180f2fc040dc5e8c18eb8461fc65
fuad <ahmadfuadoorakam@gmail.com>
2024-08-12 22:01:30 +0530

    completed 11.1 task

---
+++ index.js
@@ -0,0 +1,8 @@
+const mathFunctions = require('./utils');
+
+const { add, subtraction, multiply, division } = mathFunctions;
+
+console.log(add(10, 20));
+console.log(subtraction(10, 20));
+console.log(multiply(10, 20));
+console.log(division(10, 20));
---
+++ utils.js
@@ -0,0 +1,17 @@
+const add = (a, b) => {
+    return a + b;
+}
+
+const subtraction = (a, b) => {
+    return a - b;
+}
+
+const multiply = (a, b) => {
+    return a * b;
+}
+
+const division = (a, b) => {
+    return a / b;
+}
+
+module.exports = { add, subtraction, multiply, division };
